643421f84a651cb1127caafbbae3bda32436e384
朱文赵 <zwz@servyou.com.cn>
2019-01-23 17:47:57 +0800

    采用restful的url风格

--- src/main/Java/com/zafu/nichang/controller/ProductController.java
+++ src/main/Java/com/zafu/nichang/controller/ProductController.java
@@ -3,16 +3,20 @@ package com.zafu.nichang.controller;
 import com.zafu.nichang.service.WebSpiderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
 
 /**
  * 控制器
+ * 采用restful的url风格
+ *
  * @author 倪畅
  * 2019-01-23
  */
 @Controller
+@RequestMapping("/product")
 public class ProductController {
 
     @Autowired
@@ -20,7 +24,7 @@ public class ProductController {
 
 
     @Autowired
-    @RequestMapping("/insertProduct")
+    @PostMapping()
     public void insertProduct() {
         webSpiderService.executeSpiderProdctFromWeb();
     }
