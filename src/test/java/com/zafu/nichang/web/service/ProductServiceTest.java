33f4f5660942d8ce1b5318826a695878ac21c33e
朱文赵 <zwz@servyou.com.cn>
2019-01-23 15:56:52 +0800

    修改junit版本

---
+++ src/test/java/com/zafu/nichang/web/service/ProductServiceTest.java
@@ -0,0 +1,18 @@
+package com.zafu.nichang.web.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author 朱文赵
+ * @date 2019/1/23 15:14
+ */
+public class ProductServiceTest {
+
+
+    @Test
+    public void testCompile() {
+        System.out.println(1);
+    }
+}
